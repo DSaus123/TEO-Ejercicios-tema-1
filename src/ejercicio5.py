7246a8c28941b0d91578cd291f8db57457fed76b
DSaus123 <carlosmajaronabadia.com>
2024-10-02 11:54:27 +0200

    Commit 4

--- src/ejercicio5.py
+++ src/ejercicio5.py
@@ -1,30 +1,15 @@
 from datetime import date
 
 def calcula_dia_semana(fecha):
-    return date.weekday(fecha)
+    nombres_dias_semana = ["lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo"]
+    dia_semana = fecha.weekday()
+    return nombres_dias_semana[dia_semana]
 
-dia = int(input("Escriba el día: "))
-mes = int(input("Escriba el mes: "))
-año = int(input("Escriba el año: "))
-
-fecha = date(año, mes, dia)
-print(f"Fecha: {fecha}")
-
-res = calcula_dia_semana(fecha)
-
-if res == 0:
-    sem = "Lunes"
-elif res == 1:
-    sem = "Martes"
-elif res == 2:
-    sem = "Miércoles"
-elif res == 3:
-    sem = "Jueves"
-elif res == 4:
-    sem = " Viernes"
-elif res == 5:
-    sem = "Sábado"
-else:
-    sem = "Domingo"
-
-print(f"Día de la semana: {sem}")
+if __name__ == "__main__":
+    dia = int(input("Escriba el día: "))
+    mes = int(input("Escriba el mes: "))
+    año = int(input("Escriba el año: "))
+    fecha = date(año, mes, dia)
+    print(f"Fecha: {fecha}")
+    res = calcula_dia_semana(fecha)
+    print(f"Día de la semana: {res}")
